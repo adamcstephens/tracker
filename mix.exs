a38e4a55b12beaa099309a27e52c0f2cde502f61
Adam C. Stephens <adam@valkor.net>
2025-04-01 20:45:23 -0400

    chore: update deps

--- mix.exs
+++ mix.exs
@@ -71,7 +71,7 @@ defmodule Tracker.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
+      {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:ex_brotli, "~> 0.6.0"},
       {:req, "~> 0.5.8"}
