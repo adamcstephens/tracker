5eda1cedb4810d5d0d599616fb1ab9a3138a7314
Adam C. Stephens <adam@valkor.net>
2026-04-02 16:17:08 -0400

    dev: shrink epi instance and pg

--- .services/postgres/start.sh
+++ .services/postgres/start.sh
@@ -6,13 +6,13 @@ SOCKDIR="$PWD/.services/postgres"
 
 if [ ! -d "$DATADIR" ]; then
   initdb --username=postgres --pgdata="$DATADIR"
-  echo "unix_socket_directories = '$SOCKDIR'" >> "$DATADIR/postgresql.conf"
-  echo "listen_addresses = ''" >> "$DATADIR/postgresql.conf"
+  echo "unix_socket_directories = '$SOCKDIR'" >>"$DATADIR/postgresql.conf"
+  echo "listen_addresses = ''" >>"$DATADIR/postgresql.conf"
 fi
 
 exec postgres -D "$DATADIR" \
-  -c shared_buffers=4GB \
-  -c effective_cache_size=12GB \
+  -c shared_buffers=2GB \
+  -c effective_cache_size=4GB \
   -c maintenance_work_mem=512MB \
   -c work_mem=16MB \
   -c random_page_cost=1.1 \
